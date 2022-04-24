5db37761c6bdc5e4c846bb5f95b4c9b2683426c1
Sam <snippetsbysam@outlook.com>
2022-04-24 01:20:18 +0100

    Exercise 3.7 complete

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const app = express()
-
-app.use(express.json())
+const morgan = require('morgan')
 
 let persons = [
   { 
@@ -26,6 +25,8 @@ let persons = [
   }
 ]
 
+app.use(express.json())
+app.use(morgan('tiny'))
 
 // all persons
 app.get('/api/persons', (request, response) => {
